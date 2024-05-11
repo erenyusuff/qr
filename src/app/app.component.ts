98219c6296569c78bc3e114a4ebc1121294b83eb
Yusuf Eren <yusuf.er48en@gmail.com>
2024-05-12 01:03:00 +0300

    form created and integrated with emailjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qr';
+ form: FormGroup = this.fb.group({
+   name: '',
+   surname: '',
+   class: '',
+   department: '',
+   experience: '',
+   available_time: ''
+ })
+  constructor(private fb: FormBuilder) {}
+
+ async send(){
+   emailjs.init('jA57RRSFZKW_ovFsh')
+   let response = await emailjs.send('service_1u2y2je','template_owgxv9m', {
+     name: this.form.value.name,
+     surname: this.form.value.surname,
+     class: this.form.value.class,
+     department: this.form.value.department,
+     experience: this.form.value.experience,
+     available_time: this.form.value.available_time
+   })
+
+   alert('Başvuru talebiniz alınmıştır.')
+   this.form.reset()
 }
+
+}
+
+
