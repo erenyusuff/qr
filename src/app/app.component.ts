f1c4a7132ed05eaa8d0b4a3e0f55324f407c7a13
Yusuf Eren <yusuf.er48en@gmail.com>
2024-05-12 23:14:36 +0300

    testing custom domain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class AppComponent {
   departments: any = ['Pilotluk','Mekanik','Tasarım']
+  available_times: any = ['0-1 Saat','1-2 Saat','2-3 Saat','3 Saat ve Üzeri']
  form: FormGroup = this.fb.group( {
    name: ['', Validators.required],
    surname: ['', Validators.required],
    class: ['', Validators.required],
-   email: ['', Validators.required],
-   phone_number: ['', Validators.required],
+   email: ['', Validators.required,Validators.email],
+   phone_number: ['',[Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
    department: ['', Validators.required],
    experience: ['', Validators.required],
    available_time: ['', Validators.required],
@@ -38,7 +39,9 @@ export class AppComponent {
    alert('Başvuru talebiniz alınmıştır.')
    this.form.reset()
 }
-
+  get m(){
+    return this.form.controls;
+  }
 }
 
 
